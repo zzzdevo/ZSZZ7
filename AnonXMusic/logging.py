aa7d386c6f3a2df18c5469274d90a0de2730dd72
zzzdevo <110237161+zzzdevo@users.noreply.github.com>
2023-11-23 21:49:27 +0300

    Update logging.py

--- AnonXMusic/logging.py
+++ AnonXMusic/logging.py
@@ -13,7 +13,6 @@ logging.basicConfig(
 logging.getLogger("httpx").setLevel(logging.ERROR)
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
 logging.getLogger("pytgcalls").setLevel(logging.ERROR)
-logging.getLogger("ntgcalls").setLevel(logging.ERROR)
 
 
 def LOGGER(name: str) -> logging.Logger:
