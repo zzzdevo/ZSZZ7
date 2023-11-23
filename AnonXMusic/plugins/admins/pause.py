c0b7692cf5165b34a9dfc639c7b4e4b78da7227a
zzzdevo <110237161+zzzdevo@users.noreply.github.com>
2023-11-23 15:12:33 +0300

    Update pause.py

--- AnonXMusic/plugins/admins/pause.py
+++ AnonXMusic/plugins/admins/pause.py
@@ -1,6 +1,6 @@
 from pyrogram import filters
 from pyrogram.types import Message
-
+from strings.filters import command
 from AnonXMusic import app
 from AnonXMusic.core.call import Anony
 from AnonXMusic.utils.database import is_music_playing, music_off
@@ -9,7 +9,7 @@ from AnonXMusic.utils.inline import close_markup
 from config import BANNED_USERS
 
 
-@app.on_message(filters.command(["pause", "cpause"]) & filters.group & ~BANNED_USERS)
+@app.on_message(command(["pause", "cpause","/pause","/cpause","وسبە","وەستانی کاتی"]) & ~filters.private & ~BANNED_USERS)
 @AdminRightsCheck
 async def pause_admin(cli, message: Message, _, chat_id):
     if not await is_music_playing(chat_id):
