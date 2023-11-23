81ad8142a86d34154f424e12b45267f596d52d0a
zzzdevo <110237161+zzzdevo@users.noreply.github.com>
2023-11-23 15:13:46 +0300

    Update skip.py

--- AnonXMusic/plugins/admins/skip.py
+++ AnonXMusic/plugins/admins/skip.py
@@ -1,6 +1,6 @@
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardMarkup, Message
-
+from strings.filters import command
 import config
 from AnonXMusic import YouTube, app
 from AnonXMusic.core.call import Anony
@@ -14,7 +14,7 @@ from config import BANNED_USERS
 
 
 @app.on_message(
-    filters.command(["skip", "cskip", "next", "cnext"]) & filters.group & ~BANNED_USERS
+    command(["skip", "cskip", "next", "cnext","/skip","/next","/cskip","دواتر","سکیپ"]) & ~filters.private & ~BANNED_USERS
 )
 @AdminRightsCheck
 async def skip(cli, message: Message, _, chat_id):
